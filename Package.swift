ae3070cae06ac9383fe4f27178f38e881b93abcd
Jeff Ching <chingor@google.com>
2026-09-07 09:33:27 -0700

    deps: reference remote repo for common packages (#697)
    Towards #450 
    
    Updates references for:
    * swift-google-api
    * swift-google-gax
    * swift-google-cloud-common
    * swift-google-type
    * swift-google-rpc-context

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
     .library(name: "GoogleCloudBuildV1", targets: ["GoogleCloudBuildV1"])
   ],
   dependencies: [
-    .package(path: "../../generated/swift-google-api"),
+    .package(url: "https://github.com/googleapis/swift-google-api", from: "0.1.0-preview"),
     .package(url: "https://github.com/googleapis/swift-google-auth", from: "0.0.0-preview"),
-    .package(path: "../../packages/swift-google-gax"),
+    .package(url: "https://github.com/googleapis/swift-google-gax", from: "0.0.0-preview"),
     .package(url: "https://github.com/googleapis/swift-google-wkt", from: "0.1.0-preview"),
     .package(path: "../../generated/swift-google-longrunning"),
     .package(url: "https://github.com/googleapis/swift-google-rpc", from: "0.1.0-preview"),
